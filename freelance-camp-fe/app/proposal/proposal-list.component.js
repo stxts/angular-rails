80159c143b18befd655283911246538ff2a072a0
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-01-31 20:55:36 -0800

    added proposal model class and rendered it

--- freelance-camp-fe/app/proposal/proposal-list.component.js
+++ freelance-camp-fe/app/proposal/proposal-list.component.js
@@ -6,8 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var proposal_1 = require("./proposal");
 var ProposalListComponent = (function () {
     function ProposalListComponent() {
+        this.proposalOne = new proposal_1.Proposal(15, 'crawling_ninja', 'https://crawling.ninja', 'python', 150, 120, 15, 'st@crawling.ninja');
+        this.proposalTwo = new proposal_1.Proposal(16, 'stxts', 'https://stxts.com', 'Ruby on rails', 100, 100, 10, 'st@stxts.com');
+        this.proposalThree = new proposal_1.Proposal(17, 'bke', 'https://gohelu.com', 'c#', 170, 190, 25, 'st@bke.email');
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
     }
     return ProposalListComponent;
 }());
--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
 	moduleId: module.id,
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent{}
+export class ProposalListComponent{
+	proposalOne: Proposal = new Proposal(15, 'crawling_ninja', 'https://crawling.ninja', 'python', 150, 120, 15, 'st@crawling.ninja')
+	proposalTwo: Proposal = new Proposal(16, 'stxts', 'https://stxts.com', 'Ruby on rails', 100, 100, 10, 'st@stxts.com')
+	proposalThree: Proposal = new Proposal(17, 'bke', 'https://gohelu.com', 'c#', 170, 190, 25, 'st@bke.email')
+
+	proposals: Proposal[] = [
+	this.proposalOne, 
+	this.proposalTwo, 
+	this.proposalThree
+	]
+}
