1ceaeeeb8c1b6b9a317eb0321198d24f75690543
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-02-25 23:40:24 -0800

    created document service, set up observable rxjs

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DocumentsComponent } from './documents/documents.component';
+import { DocumentService } from './documents/document.service';
 import { ProposalListComponent } from './proposal/proposal-list.component';
 import { ProposalNewComponent } from './proposal/proposal-new.component';
 import { ProposalShowComponent } from './proposal/proposal-show.component';
@@ -24,6 +25,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 		ProposalListComponent,
 		ProposalNewComponent,
 		ProposalShowComponent
+	],
+	providers: [
+		DocumentService
 	],
 	bootstrap: [
 		AppComponent
